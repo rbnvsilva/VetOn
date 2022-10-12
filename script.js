d1f99d4bb19d12a302bb71feab333069e25c6f4d
RodrigoSimoes-22001628 <a22001628@alunos.ulht.pt>
2022-10-12 14:45:45 +0100

    adicao relogio no footer

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-function clock() {
+function renderTime() {
     var myDate = new Date();
     var day = myDate.getDate();
     var month = myDate.getMonth();
     var monthArray = ['Janeiro','Fevereiro','Marco','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
     var year = myDate.getYear();
     if (year < 1000) {
-    year += 1900;
+      year += 1900;
     }
-    var currentTime = new Date();        
-    var horas = currentTime.getHours();
-    var minutos = currentTime.getMinutes();
-    var segundos = currentTime.getSeconds();
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var m = currentTime.getMinutes();
+    var s = currentTime.getSeconds();
 
-    if (horas == 24) {
-    horas = 0;
-    } else if (horas > 12) {
-    horas = horas - 0;
-    }
-
-    if (horas < 10) {
-    horas = "0" + horas;
-    }
-    if (minutos < 10) {
-    minutos = "0" + minutos;
-    }
-    if (segundos < 10) {
-    segundos = "0" + segundos;
+    if (h == 24) {
+      h = 0;
+    } else if (h > 12) {
+      h = h - 0;
     }
 
-    var clock = document.getElementById("relogio");
-    clock.textContent = "" + day + " " + monthArray[month] + " " + year + " | " + horas + ":" + minutos + ":" + segundos;
-    clock.innerText = "" + day + " " + monthArray[month] + " " + year + " | " + horas + ":" + minutos + ":" + segundos;
+    if (h < 10) {
+      h = "0" + h;
+    }
+    if (m < 10) {
+      m = "0" + m;
+    }
+    if (s < 10) {
+      s = "0" + s;
+    }
 
-    setTimeout("clock()", 1000)
-}
-clock();
+    var clock = document.getElementById("clock");
+    clock.textContent = "" + day + " " + monthArray[month] + " " + year + " | " + h + ":" + m + ":" + s;
+    clock.innerText = "" + day + " " + monthArray[month] + " " + year + " | " + h + ":" + m + ":" + s;
+
+    setTimeout("renderTime()", 1000)
+  }
+  renderTime();
   
